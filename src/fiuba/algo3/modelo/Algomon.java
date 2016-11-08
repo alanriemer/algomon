15524a017e5cffe65dc485275b751a042dfd8343
cotarelorodrigo <cotarelorodrigo@gmail.com>
2016-11-08 18:58:22 -0300

    Ultimo test de commit

--- src/fiuba/algo3/modelo/Algomon.java
+++ src/fiuba/algo3/modelo/Algomon.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import fiuba.algo3.modelo.ataques.Ataque;
 
 public abstract class Algomon {
-	protected int healthPoints; //Test rodrigo
+	protected int healthPoints; //Test rodrigo segundo test
 	protected Tipo tipo;
 	protected HashMap<String,Ataque> moveSet = new HashMap<String,Ataque>();
 	protected HashMap<Tipo,Double> weakAndStr = new HashMap<Tipo,Double>();
