3920f489cd5db0fa5947f5729c2722a2c456b403
Leandro Denis <leandroagustindenis@gmail.com>
2016-11-08 13:13:50 -0300

    Agregados test en Bulbasaur por tipo, faltando los test de Chupavidas

--- test/fiuba/algo3/tests/BulbasaurTest.java
+++ test/fiuba/algo3/tests/BulbasaurTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import fiuba.algo3.modelo.Bulbasaur;
+import fiuba.algo3.modelo.Charmander;
+import fiuba.algo3.modelo.Rattata;
+import fiuba.algo3.modelo.Squirtle;
 
 public class BulbasaurTest {
 	@Test
@@ -27,5 +30,71 @@ public class BulbasaurTest {
 		unBulbasaur.atacaConAtaqueRapidoAlEnemigo(otroBulbasaur);
 		
 		assertEquals(otroBulbasaur.getHealthPoints(),vida-10);
+	}
+	
+	@Test
+	public void test03BulbasaurRealizaLatigoCepaContraSquirtleYHace30deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Squirtle otroSquirtle = new Squirtle();
+		
+		int vida = otroSquirtle.getHealthPoints();
+		unBulbasaur.atacaConLatigoCepaAlEnemigo(otroSquirtle);
+		
+		assertEquals(otroSquirtle.getHealthPoints(),vida-30);
+	}
+	
+	@Test
+	public void test04BulbasaurRealizaAtaqueRapidoContraSquirtleYHace10deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Squirtle unSquirtle = new Squirtle();
+		
+		int vida = unSquirtle.getHealthPoints();
+		unBulbasaur.atacaConAtaqueRapidoAlEnemigo(unSquirtle);
+		
+		assertEquals(unSquirtle.getHealthPoints(),vida-10);
+	}
+	
+	@Test
+	public void test05BulbasaurRealizaLatigoCepaContraCharmanderYHace7deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Charmander unCharmander = new Charmander();
+		
+		int vida = unCharmander.getHealthPoints();
+		unBulbasaur.atacaConLatigoCepaAlEnemigo(unCharmander);
+		
+		assertEquals(unCharmander.getHealthPoints(),vida-7);
+	}
+	
+	@Test
+	public void test06BulbasaurRealizaAtaqueRapidoContraCharmanderYHace10deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Charmander unCharmander = new Charmander();
+		
+		int vida = unCharmander.getHealthPoints();
+		unBulbasaur.atacaConAtaqueRapidoAlEnemigo(unCharmander);
+		
+		assertEquals(unCharmander.getHealthPoints(),vida-10);
+	}
+	
+	@Test
+	public void test07BulbasaurRealizaLatigoCepaContraRattataYHace15deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Rattata unRattata = new Rattata();
+		
+		int vida = unRattata.getHealthPoints();
+		unBulbasaur.atacaConLatigoCepaAlEnemigo(unRattata);
+		
+		assertEquals(unRattata.getHealthPoints(),vida-15);
+	}
+	
+	@Test
+	public void test08BulbasaurRealizaAtaqueRapidoContraRattataYHace10deDanio(){
+		Bulbasaur unBulbasaur = new Bulbasaur();
+		Rattata unRattata = new Rattata();
+		
+		int vida = unRattata.getHealthPoints();
+		unBulbasaur.atacaConAtaqueRapidoAlEnemigo(unRattata);
+		
+		assertEquals(unRattata.getHealthPoints(),vida-10);
 	}
 }
