10d5acf74a32a169d8db1287152ebe4c0a70ba42
Leandro Denis <leandroagustindenis@gmail.com>
2016-11-09 12:44:39 -0300

    Arreglados test que no pasaban en AtaquesSimplesTest

--- src/fiuba/algo3/modelo/Chansey.java
+++ src/fiuba/algo3/modelo/Chansey.java
@@ -5,17 +5,16 @@ import fiuba.algo3.modelo.ataques.Ataque;
 public class Chansey extends Algomon {
 	public Chansey (){
 		puntosVida = 130;
-		tipo = Tipo.Planta;
+		tipo = Tipo.Normal;
 		Ataque latigoCepa = new Ataque(15,10, Tipo.Planta);
 		moveSet.put("Latigo Cepa",latigoCepa);
-		weakAndStr.put(Tipo.Agua, 0.5);
-		weakAndStr.put(Tipo.Fuego, 2.0);
-		weakAndStr.put(Tipo.Planta, 0.5);
+		weakAndStr.put(Tipo.Agua, 1.0);
+		weakAndStr.put(Tipo.Fuego, 1.0);
+		weakAndStr.put(Tipo.Planta, 1.0);
 		weakAndStr.put(Tipo.Normal, 1.0);		
 	}
 
 	public void atacarConLatigoCepa(Algomon enemigo) {
-		// TODO Auto-generated method stub
-		
+		enemigo.recibirAtaque(moveSet.get("Latigo Cepa"));
 	}
 }
--- src/fiuba/algo3/modelo/Jigglypuff.java
+++ src/fiuba/algo3/modelo/Jigglypuff.java
@@ -5,13 +5,13 @@ import fiuba.algo3.modelo.ataques.Ataque;
 public class Jigglypuff extends Algomon{
 	public Jigglypuff(){
 		puntosVida = 130;
-		tipo = Tipo.Planta;
+		tipo = Tipo.Normal;
 		puntosVida = 140;
 		Ataque latigoCepa = new Ataque(15,10, Tipo.Planta);
 		moveSet.put("Latigo Cepa",latigoCepa);
-		weakAndStr.put(Tipo.Agua, 0.5);
-		weakAndStr.put(Tipo.Fuego, 2.0);
-		weakAndStr.put(Tipo.Planta, 0.5);
+		weakAndStr.put(Tipo.Agua, 1.0);
+		weakAndStr.put(Tipo.Fuego, 1.0);
+		weakAndStr.put(Tipo.Planta, 1.0);
 		weakAndStr.put(Tipo.Normal, 1.0);		
 	}
 
