d5e930e1c3e1b42ab01db165eca542175437231a
Alan Riemer <alanriemer@gmail.com>
2016-11-08 22:38:50 -0300

    Borro esta clase, no hace nada.

--- src/fiuba/algo3/modelo/CalcularDanio.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.modelo;
-
-public class CalcularDanio {
-	
-	public static int calcularPorTipo(Tipo tipo, int potencia, Algomon enemigo){
-		double multiplicador = enemigo.weakAndStr.get(tipo);
-		return (int) Math.floor(potencia * multiplicador);
-	}
-}
