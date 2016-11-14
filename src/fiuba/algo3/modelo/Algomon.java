cdc3341d85650ac0f80d7c1190c73362b59d8f11
Leandro Denis <leandroagustindenis@gmail.com>
2016-11-14 13:54:14 -0300

    Agregados Test En EStadoTest

--- src/fiuba/algo3/modelo/Algomon.java
+++ src/fiuba/algo3/modelo/Algomon.java
@@ -84,5 +84,4 @@ public abstract class Algomon {
 		return estado.turnos();
 	}
 
-
 }
--- test/fiuba/algo3/tests/EstadoAlgomonTest.java
+++ test/fiuba/algo3/tests/EstadoAlgomonTest.java
@@ -43,7 +43,7 @@ public class EstadoAlgomonTest {
 		unChansey.atacarCon("Canto",unBulbasaur);
 		
 		assertEquals(unBulbasaur.estaDormido(),true);
-		//assertEquals(unBulbasaur.estaQuemado(),true);
+		assertEquals(unBulbasaur.estaQuemado(),true);
 	}
 	
 	@Test
@@ -100,5 +100,46 @@ public class EstadoAlgomonTest {
 		
 		assertEquals(unBulbasaur.estaDormido(),false);
 		assertEquals(unChansey.getPuntosVidaActuales(), 120);
+	}
+	
+	@Test
+	public void test08BulbasaurDuermeyLuegoPasaAQuemadoDormidoYLuegoPasaSoloAQuemado() throws AtaqueNoDisponibleException{
+		Bulbasaur unBulbasaur = new Bulbasaur();		
+		Chansey unChansey = new Chansey();
+		Charmander unCharmander = new Charmander();
+		
+		unChansey.atacarCon("Canto",unBulbasaur);
+		//Ataques que realiza mientras duerme y no hacen daño
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+		//Pasa a quemado dormido
+		unCharmander.atacarCon("Fogonazo", unBulbasaur);	
+		assertEquals(unBulbasaur.estaDormidoQuemado(),true);
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+
+		//Despierta y realiza ataque con daño
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+		
+		assertEquals(unBulbasaur.estaQuemado(),true);
+		assertEquals(unBulbasaur.estaDormido(),false);
+		assertEquals(unChansey.getPuntosVidaActuales(), 120);
+	}
+	
+	@Test
+	public void test09BulbasaurPasaAQuemadoDormidoYRecibeDanio() throws AtaqueNoDisponibleException{
+		Bulbasaur unBulbasaur = new Bulbasaur();		
+		Charmander unCharmander = new Charmander();
+		Chansey unChansey = new Chansey();
+		
+		unChansey.atacarCon("Canto",unBulbasaur);			
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+		unBulbasaur.atacarCon("Ataque rapido",unChansey);
+		unCharmander.atacarCon("Fogonazo", unBulbasaur);	
+		assertEquals(unBulbasaur.estaDormidoQuemado(),true);
+		int vidaAntes = unBulbasaur.getPuntosVidaActuales();
+		unBulbasaur.atacarCon("Latigo Cepa",unChansey);
+		int vidaDespues = unBulbasaur.getPuntosVidaActuales();
+		//14 es el 10% de la vida de Bulbasaur (140)
+		assertEquals(vidaDespues, vidaAntes-14);
 	}
 }
